7171b5dd105e878f1383fe5dcbf22d4e46e205c7
abdipriyangga <mabdipme@gmail.com>
2021-12-04 00:07:05 +0700

    slicing full profile screen with function button signout

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -11,8 +11,9 @@ import {
 import { DefaultUser } from '../assets';
 import FaIcon from 'react-native-vector-icons/FontAwesome5';
 import Icon from 'react-native-vector-icons/Ionicons';
-
-const Profile = () => {
+import { connect } from 'react-redux';
+import { authLogout } from '../redux/actions/auth';
+const Profile = props => {
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -276,12 +277,225 @@ const Profile = () => {
             </TouchableOpacity>
           </View>
         </View>
+
+        {/* Keamanan */}
+        <View style={styles.wrapHeaderSecurity}>
+          <View style={{ marginTop: 30 }}>
+            <Text style={{ fontSize: 20, fontWeight: '800', color: '#000' }}>
+              Keamanan
+            </Text>
+          </View>
+          <View style={{ marginTop: 30 }}>
+            {/* Info Profile */}
+            <TouchableOpacity
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View style={{ flexDirection: 'row', marginVertical: 0 }}>
+                <FaIcon
+                  name="lock"
+                  size={25}
+                  style={{ marginHorizontal: 10 }}
+                />
+                <Text
+                  style={{
+                    color: '#000',
+                    fontWeight: '800',
+                    fontSize: 16,
+                    marginHorizontal: 20,
+                  }}>
+                  Ubah Password
+                </Text>
+              </View>
+              <Icon
+                name="chevron-forward-outline"
+                size={25}
+                style={{ marginTop: 0 }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* Tentang */}
+        <View style={styles.wrapHeaderAbout}>
+          <View style={{ marginTop: 30 }}>
+            <Text style={{ fontSize: 20, fontWeight: '800', color: '#000' }}>
+              Tentang
+            </Text>
+          </View>
+          <View style={{ marginTop: 30 }}>
+            {/* Info Profile */}
+            <TouchableOpacity
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View style={{ flexDirection: 'row', marginVertical: 0 }}>
+                <FaIcon
+                  name="trophy"
+                  size={25}
+                  style={{ marginHorizontal: 10 }}
+                />
+                <Text
+                  style={{
+                    color: '#000',
+                    fontWeight: '800',
+                    fontSize: 16,
+                    marginHorizontal: 20,
+                  }}>
+                  Keuntungan Pakai ODO
+                </Text>
+              </View>
+              <Icon
+                name="chevron-forward-outline"
+                size={25}
+                style={{ marginTop: 0 }}
+              />
+            </TouchableOpacity>
+            <View
+              style={{
+                marginVertical: 13,
+                borderWidth: 1,
+                borderBottomColor: '#e3e3e3',
+                opacity: 0.1,
+              }}
+            />
+            {/* ODO Premier and Points */}
+            <TouchableOpacity
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View style={{ flexDirection: 'row', marginVertical: 0 }}>
+                <FaIcon
+                  name="lightbulb"
+                  size={25}
+                  style={{ marginHorizontal: 10 }}
+                />
+                <Text
+                  style={{
+                    color: '#000',
+                    fontWeight: '800',
+                    fontSize: 16,
+                    marginHorizontal: 30,
+                  }}>
+                  Panduan ODO
+                </Text>
+              </View>
+              <Icon
+                name="chevron-forward-outline"
+                size={25}
+                style={{ marginTop: 0 }}
+              />
+            </TouchableOpacity>
+            <View
+              style={{
+                marginVertical: 13,
+                borderWidth: 1,
+                borderBottomColor: '#e3e3e3',
+                opacity: 0.1,
+              }}
+            />
+            <TouchableOpacity
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View style={{ flexDirection: 'row', marginVertical: 0 }}>
+                <FaIcon
+                  name="file-alt"
+                  size={25}
+                  style={{ marginHorizontal: 10 }}
+                />
+                <Text
+                  style={{
+                    color: '#000',
+                    fontWeight: '800',
+                    fontSize: 16,
+                    marginHorizontal: 28,
+                  }}>
+                  Syarat dan Ketentuan
+                </Text>
+              </View>
+              <Icon
+                name="chevron-forward-outline"
+                size={25}
+                style={{ marginTop: 0 }}
+              />
+            </TouchableOpacity>
+            <View
+              style={{
+                marginVertical: 13,
+                borderWidth: 1,
+                borderBottomColor: '#e3e3e3',
+                opacity: 0.1,
+              }}
+            />
+            <TouchableOpacity
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View style={{ flexDirection: 'row', marginVertical: 0 }}>
+                <FaIcon
+                  name="shield-alt"
+                  size={25}
+                  style={{ marginHorizontal: 10 }}
+                />
+                <Text
+                  style={{
+                    color: '#000',
+                    fontWeight: '800',
+                    fontSize: 16,
+                    marginHorizontal: 25,
+                  }}>
+                  Kebijakan Privasi
+                </Text>
+              </View>
+              <Icon
+                name="chevron-forward-outline"
+                size={25}
+                style={{ marginTop: 0 }}
+              />
+            </TouchableOpacity>
+            <View
+              style={{
+                marginVertical: 13,
+                borderWidth: 1,
+                borderBottomColor: '#e3e3e3',
+                opacity: 0.1,
+              }}
+            />
+            <TouchableOpacity
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <View style={{ flexDirection: 'row', marginVertical: 0 }}>
+                <FaIcon
+                  name="question-circle"
+                  size={25}
+                  style={{ marginHorizontal: 10 }}
+                />
+                <Text
+                  style={{
+                    color: '#000',
+                    fontWeight: '800',
+                    fontSize: 16,
+                    marginHorizontal: 25,
+                  }}>
+                  Pusat Bantuan
+                </Text>
+              </View>
+              <Icon
+                name="chevron-forward-outline"
+                size={25}
+                style={{ marginTop: 0 }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={props.authLogout}
+            style={styles.btnSignout}>
+            <Text style={styles.textBtn}>Signout</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </View>
   );
 };
-
-export default Profile;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+const mapDispatchToProps = {
+  authLogout,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -299,9 +513,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 25,
   },
+  wrapHeaderSecurity: {
+    backgroundColor: '#fff',
+    padding: 25,
+    marginVertical: 20,
+  },
+  wrapHeaderAbout: {
+    backgroundColor: '#fff',
+    padding: 25,
+    marginBottom: 20,
+  },
   imageProfile: {
     width: 50,
     height: 50,
     borderRadius: 15,
+  },
+  btnSignout: {
+    top: 0,
+    marginHorizontal: '8%',
+    width: 340,
+    height: 80,
+    backgroundColor: '#6604c2',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 40,
+  },
+  textBtn: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
